6d7099f94e3d0a6c97feb1a7e355939bd734e21e
root <root@localhost.localdomain>
2019-12-31 11:01:44 +0000

    update

--- delivery.c
+++ delivery.c
@@ -40,3 +40,46 @@ int str_to_hex(void)
 	return 0;
 }
 
+#include <stdlib.h>
+#include <unistd.h>           // close()
+#include <string.h>           // strcpy, memset(), and memcpy()
+#include <netdb.h>            // struct addrinfo
+#include <sys/types.h>        // needed for socket(), uint8_t, uint16_t, uint32_t
+#include <sys/socket.h>       // needed for socket()
+#include <netinet/in.h>       // IPPROTO_ICMP, INET_ADDRSTRLEN
+#include <netinet/ip.h>       // struct ip and IP_MAXPACKET (which is 65535)
+#include <netinet/ip_icmp.h>  // struct icmp, ICMP_ECHO
+#include <arpa/inet.h>        // inet_pton() and inet_ntop()
+#include <sys/ioctl.h>        // macro ioctl is defined
+#include <net/if.h>           // struct ifreq
+#include <linux/if_ether.h>   // ETH_P_IP = 0x0800, ETH_P_IPV6 = 0x86DD
+#include <linux/if_packet.h>  // struct sockaddr_ll (see man 7 packet)
+#include <net/ethernet.h>
+#include <errno.h>            // errno, perror()
+
+int  get_src_mac_by_str(void) 
+{
+	// 通过网卡名获取源地址
+	char * interface = "enp0s3";
+	int sd;
+	uint8_t src_mac[6];
+	struct ifreq ifr;
+	if ((sd = socket (PF_PACKET, SOCK_RAW, htons (ETH_P_ALL))) < 0) {
+	        perror ("socket() failed to get socket descriptor for using ioctl() ");
+	        exit (EXIT_FAILURE);
+	}
+	snprintf (ifr.ifr_name, sizeof (ifr.ifr_name), "%s", interface);
+	if (ioctl (sd, SIOCGIFHWADDR, &ifr) < 0) {
+		perror ("ioctl() failed to get source MAC address ");
+		return (EXIT_FAILURE);
+	}
+	close (sd);
+	memcpy (src_mac, ifr.ifr_hwaddr.sa_data, sizeof(src_mac));
+	
+	printf ("MAC address for interface %s is ", interface);
+	int i = 0;
+	for(i = 0; i < sizeof(src_mac); i++){
+		printf( i==5 ? "%.2x\n" : "%.2x:", src_mac[i]);
+	}
+	return (EXIT_SUCCESS);
+}
--- test.c
+++ test.c
@@ -9,7 +9,7 @@
 #define PRINT_YELLOW(str)   printf("\033[0;33m%s\033[0;39m", str);
 #define PRINT_BULE(str)     printf("\033[0;34m%s\033[0;39m", str);
 #define PRINT_PURPLE(str)   printf("\033[0;35m%s\033[0;39m", str);
-#define PRINT_SEPARATOR     PRINT_BULE("---------------------------------------------------------\n")
+#define PRINT_SEPARATOR     PRINT_BULE("-------------------------------------------------------------\n")
 #define BULE_DIVISION       "\033[0;34m|\033[0;39m"
 
 #define DELIVER(func,is_run,delivery,is_show_desc,desc)  deliver(func, is_run, #func, delivery_c[delivery],is_show_desc, desc);
@@ -29,7 +29,7 @@
 	PRINT_SEPARATOR  \
 } while(0);
 #define PRINT_INFO          printf("%*.*s %s 是否执行: %*.*s %s 进度: %*.*s\n", \
-		16, 16,  func_name, BULE_DIVISION, 8, 6, is_run ? "执行" : "忽略", BULE_DIVISION, 16, 12, delivery_c);
+		20, 20,  func_name, BULE_DIVISION, 8, 6, is_run ? "执行" : "忽略", BULE_DIVISION, 16, 12, delivery_c);
 
 typedef int (*deliver_cb)(void);
 
@@ -67,6 +67,7 @@ void main(void)
 	DELIVER(go_test,              0, goodProgress, 0, "测试回调函数")
 	DELIVER(data_length,          0, goodProgress, 0, "计算数据长度")
 	DELIVER(str_to_hex,           0, goodProgress, 0, "字符串转16进制")
+	DELIVER(get_src_mac_by_str,   1, goodProgress, 0, "获取网卡enp0s3的地址")
 	PRINT_PURPLE("===============================================================================\n")
 	PRINT_SEPARATOR
 	DELIVER(send_oam_ais_pdu,     0, goodProgress, 0, "获取网卡enp0s3的地址并从该网卡发送Y1731 AIS报文")
